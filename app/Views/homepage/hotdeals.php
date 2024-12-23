b025ea5fb7b0b22394c4a0869df4ad0b1d3bb4a2
Miftahus Surur Sutowo <62152669+miftahozuki@users.noreply.github.com>
2024-12-23 13:49:30 +0700

    halaman 2
    tampilan desktop

--- app/Views/homepage/hotdeals.php
+++ app/Views/homepage/hotdeals.php
@@ -19,6 +19,97 @@
 </div>
 <!-- End of Banner -->
 
+<!-- Start of Pricelist -->
+<div class="pricelist-hotdeals">
+    <h2 class="text-uppercase">Price List</h2>
+    <div class="container">
+        <div class="row justify-center mt-3">
+            <div class="col-2 my-auto">
+                <a class="button-monthly" href="#">1 MONTH</a>
+                <a class="button-monthly" href="#">1 MONTH</a>
+                <a class="button-monthly" href="#">1 MONTH</a>
+                <a class="button-monthly" href="#">1 YEAR</a>
+            </div>
+            <div class="col">
+                <div class="pricelist-card">
+                    <div class="pricelist-card-body mx-4">
+                        <h5 class="text-uppercase">If activated your account from website:</h5>
+                        <div class="row justify-center">
+                            <div class="col pricelist-button">
+                                <a class="btn-pricelist-white-lg" href="">REFFERAL</a>
+                                <a class="btn-pricelist-white" href="">250 EURO</a>
+                                <a class="btn-pricelist-white" href="">250 EURO</a>
+                                <a class="btn-pricelist-white" href="">250 EURO</a>
+                                <a class="btn-pricelist-white" href="">250 EURO</a>
+                            </div>
+                            <div class="col pricelist-button">
+                            <a class="btn-pricelist-black-lg" href="">WEBSITE</a>
+                                <a class="btn-pricelist-black" href="">250 EURO</a>
+                                <a class="btn-pricelist-black" href="">250 EURO</a>
+                                <a class="btn-pricelist-black" href="">250 EURO</a>
+                                <a class="btn-pricelist-black" href="">250 EURO</a>
+                            </div>
+                        </div>
+                        <a href="" class="btn-promo text-uppercase">GET THE PROMO <span class="d-block">FROM THE WEB</span></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+            <div class="pricelist-card">
+                    <div class="pricelist-card-body mx-4">
+                        <h5 class="text-uppercase">If activated your account from app:</h5>
+                        <div class="row justify-center">
+                            <div class="col pricelist-button">
+                                <a class="btn-pricelist-black-lg text-white" href="">ANDROID</a>
+                                <a class="btn-pricelist-black" href="">300 EURO</a>
+                                <a class="btn-pricelist-black" href="">825 EURO</a>
+                            </div>
+                            <div class="col pricelist-button">
+                            <a class="btn-pricelist-black-lg text-white" href="">IOS</a>
+                                <a class="btn-pricelist-black" href="">300 EURO</a>
+                                <a class="btn-pricelist-black" href="">825 EURO</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End of Pricelist -->
+
+<!-- Start of Why -->
+<div class="why-hotdeals">
+    <h2 class="text-uppercase">Why is it important to activate <span class="d-block">your account from our
+            website?</span></h2>
+    <div class="wrapper-why-hotdeals">
+        <div class="row justify-content-center">
+            <div class="col ms-5">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title text-white fs-4 fw-bold">EXCLUSIVE CONTENT</h5>
+                        <p class="card-text">Only for those who activate the <span class="d-block">account directly from
+                                the website!</span></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col me-5">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title text-white fs-4 fw-bold">REFFERAL BONUS</h5>
+                        <p class="card-text">Use a referral code or a referral link during the <span
+                                class="d-block">account’s activation to get an additional</span> discount!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a href="#" class="btn-activate text-uppercase">Activate Your Account <span class="d-block">with a
+                Discount!</span></a>
+    </div>
+</div>
+</div>
+<!-- End of Why -->
+
 <!-- Start of Refferal -->
 <div class="account-activation-hotdeals">
     <div class="benefit-activation">
@@ -52,29 +143,78 @@
         <div class="row">
             <div class="d-none d-lg-block col-1"></div>
             <div class="col-12 col-lg-5 order-1 order-lg-0 mt-4 mt-4 mt-lg-0">
-            <video autoplay="" preload="" muted="" loop="" playsinline="">
-                <source src="<?= BASE_URL ?>assets/vid/refferal.webm" type="video/webm">
-            </video>
+                <video autoplay="" preload="" muted="" loop="" playsinline="">
+                    <source src="<?= BASE_URL ?>assets/vid/refferal.webm" type="video/webm">
+                </video>
             </div>
             <div class="d-none d-lg-block col-1"></div>
             <div class="col-12 col-lg-5 order-0 order-lg-1 px-5">
                 <ul class="list-benefits">
                     <li>$25 for Each Monthly Renewal: <p class="text-white">
-                        For every month your referral renews their subscription, you receive $25 !    (Available via Web and App)
-                    </p></li>
+                            For every month your referral renews their subscription, you receive $25 ! (Available via
+                            Web and App)
+                        </p>
+                    </li>
                     <li>$75 for Quarterly Plans: <p class="text-white">
-                    If a friend chooses a 3-month plan, you earn $75 immediately and another $75 for each quarterly renewal.   (Available via web and app)
-                    </p></li>
+                            If a friend chooses a 3-month plan, you earn $75 immediately and another $75 for each
+                            quarterly renewal. (Available via web and app)
+                        </p>
+                    </li>
                     <li>$150 for Semi-Annual Plans: <p class="text-white">
-                    For a 6-month subscription, receive $150 immediately and another $150 for every semi-annual renewal.  (Available only via web)
-                    </p></li>
-                    <li>$300 for Annual Plans: <p class="text-white">If a friend chooses the annual plan, you get $300 immediately and another $ 300 for every annual renewal!   (Available only via web)</p></li>
+                            For a 6-month subscription, receive $150 immediately and another $150 for every semi-annual
+                            renewal. (Available only via web)
+                        </p>
+                    </li>
+                    <li>$300 for Annual Plans: <p class="text-white">If a friend chooses the annual plan, you get $300
+                            immediately and another $ 300 for every annual renewal! (Available only via web)</p>
+                    </li>
                 </ul>
             </div>
         </div>
     </div>
 </div>
 <!-- End of Sharing -->
+
+<!-- Start of Credit -->
+<div class="credit-hotdeals">
+    <div class="credit-timeline">
+        <div class="ms-5">
+            <h2 class="text-uppercase pt-3 fw-bold">Credit Timelines</h2>
+            <p class="mt-5">All the activations made between September 1st and 30th will be credited to
+                <span class="d-block">your referral balance on November 15th.</span>
+            </p>
+            <p class="mt-2">From your referral balance dashboard on the website, you can choose how to
+                <span class="d-block">withdraw your earnings:</span>
+            </p>
+            <ul class="credit-list">
+                <li>Domestic Bank Transfer <span class="text-white">to an American BANK account.</span></li>
+                <li>International Bank Transfer <span class="text-white">TO any global account.</span></li>
+                <li>Cryptocurrencies in USDT (TRC20) <span class="text-white">for fast and secure transfers.</span></li>
+            </ul>
+            <p>⚠️ Important Note: Transfer fees will be borne by the user withdrawing the balance.</p>
+        </div>
+        <img class="credit-image" src="<?= BASE_URL ?>assets/img/credit-hotdeals.png" alt="Credit Timeline Background">
+    </div>
+    <div class="download-app">
+        <h2 class="text-uppercase fw-bold">Download the App for FREE and <span class="d-block">Start Earning</span></h2>
+        <div>
+            <p class="mt-2 mb-4 fs-5">After activating your account, download the Satoshi Signal app from:</p>
+            <div class="download-button">
+                <a href="#">
+                    <img src="<?= BASE_URL ?>assets/img/btn-playstore.png" alt="Download from Play Store">
+                </a>
+                <a href="#">
+                    <img src="<?= BASE_URL ?>assets/img/btn-appstore.png" alt="Download from App Store">
+                </a>
+                <a href="#">
+                    <img src="<?= BASE_URL ?>assets/img/btn-apk.png" alt="Download APK">
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<!-- Enf of Credit -->
 
 <!-- Start of Become -->
 <div class="become-hotdeals">
