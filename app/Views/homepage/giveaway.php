a11bb80dd5a3f0a09ee1e224ec1bb323882aebe2
Miftahus Surur Sutowo <62152669+miftahozuki@users.noreply.github.com>
2024-12-25 16:24:17 +0700

    halaman 4 done

--- app/Views/homepage/giveaway.php
+++ app/Views/homepage/giveaway.php
@@ -9,6 +9,37 @@
 
 <!-- End of Banner -->
 
+<!-- Start of Giveaway -->
+<div class="giveaway">
+        <div class="row">
+            <div class="col">
+                <div class="giveaway-card">
+                    <img class="gift-image-1" src="<?= BASE_URL ?>/assets/img/140.png" alt="">
+                    <div class="giveaway-card-header">
+                        <h2>GIVEAWAY 1 <span style="color: #BFA573;">"Referral Success"</span></h2>
+                    </div>
+                    <div class="giveaway-card-body">
+                    Make a Reel by tagging Satoshi Signal Instagram page where you show: how many subscribers you have achieved by sharing your Referral Link and the earnings you have obtained. 
+
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+            <div class="giveaway-card">
+                <img class="gift-image-2" src="<?= BASE_URL ?>/assets/img/140.png" alt="">
+                    <div class="giveaway-card-header">
+                        <h2>GIVEAWAY 1 <span style="color: #BFA573;">"Referral Success"</span></h2>
+                    </div>
+                    <div class="giveaway-card-body">
+                    Make a Reel by tagging Satoshi Signal Instagram page where you show: how many subscribers you have achieved by sharing your Referral Link and the earnings you have obtained. 
+
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+<!-- End of Giveaway -->
+
 <!-- Start of Regulation -->
 <div class="regulation-giveaway">
     <div class="wrapper-regulation-giveaway">
@@ -20,6 +51,15 @@
             <li>At the end of the giveaway, the best Reels will be reposted on Satoshi Signal’s Instagram page and tagged as winners. They will then receive the prize won in USDT, network TRC-20, in the wallet added in the Referral section.
             </li>
         </ul>
+        <div class="important-notes">
+            <img class="gift-image" src="<?= BASE_URL ?>/assets/img/139.png" alt="">
+            <h2>IMPORTANT NOTES:</h2>
+            <ul>
+                <li>The timing of the giveaway, the duration, the amount awarded to the winners and the number of winners will be decided at our sole discretion.</li>
+                <li>The giveaway result does not compromise your monthly earnings of 25 USD on renewal of subscriptions subscribed via your referral link.</li>
+            </ul>
+            <a class="btn-subscribes" href="#">subscribe and Win USDT!</a>
+        </div>
     </div>
 </div>
 <!-- End of Regulation -->
