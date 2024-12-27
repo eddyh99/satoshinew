2db547675926fd4fd3299a7dca8c65cef40ba671
Miftahus Surur Sutowo <62152669+miftahozuki@users.noreply.github.com>
2024-12-27 15:35:00 +0700

    revisi mode desktop homepage
    #2

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -35,7 +35,7 @@ abstract class BaseController extends Controller
      *
      * @var list<string>
      */
-    protected $helpers = ["form", "pnglobal_helpers", "mail_template_helpers"];
+    protected $helpers = ["form", "pnglobal_helpers", "mail_template_helpers", "url"];
     /**
      * Be sure to declare properties for any property fetch you initialized.
      * The creation of dynamic property is deprecated in PHP 8.2.
